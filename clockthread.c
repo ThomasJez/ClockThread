49ff4a5c5b531b4887a22841255764a102e04364
Thomas Jez <thomas.jez@gmx.de>
2015-10-17 15:33:14 +0200

    some fixing and refactoring

--- clockthread.c
+++ clockthread.c
@@ -15,9 +15,6 @@
 //#include <termios.h>
 //#include <unistd.h>
 
-static int line;
-static int stop;
-
 struct clock_struct {
 	time_t start_time;
 	int x, y;
@@ -27,27 +24,52 @@ struct clocks_struct {
 	struct clock_struct* clock;
 	int anz_clocks;
 };
-static struct clocks_struct clock_args;
+
 static pthread_t clock_thread = 0;
+static int line;
+static int stop;
 
 zend_class_entry *clockthread_class;
 
-/*
- * The function which runs the clocks
- */
-
-static void *clock_loop(void* _clock_args) {
+static void *clock_loop(void* _clock_array) {
 	int x, y, i;
-	struct clocks_struct *clock_args = (struct clocks_struct*) _clock_args;
+	char *key;
+	uint key_len;
+	int array_count;
+	HashTable *arr_hash;
+	HashPosition pointer;
+	zval **data;
+	ulong index;
+	static struct clocks_struct clock_args;
+
+	zval* clock_array = (zval*) _clock_array;
+	arr_hash = Z_ARRVAL_P(clock_array);
+	array_count = zend_hash_num_elements(arr_hash);
+	clock_args.clock = calloc(array_count, sizeof(struct clock_struct));
+	clock_args.anz_clocks = array_count;
+
+	i = 0;
+
+	for(zend_hash_internal_pointer_reset_ex(arr_hash, &pointer);
+			zend_hash_get_current_data_ex(arr_hash, (void**) &data, &pointer) == SUCCESS;
+			zend_hash_move_forward_ex(arr_hash, &pointer)) {
+
+		zend_hash_get_current_key_ex(arr_hash, &key, &key_len, &index, 0, &pointer);
+		clock_args.clock[i].start_time = Z_LVAL_PP(data);
+		clock_args.clock[i].x = 62;
+		clock_args.clock[i].y = index + 4;
+		i++;
+	}
+
 	while (stop == 0) {
 		time_t now;
 		time(&now);
 		long dauer;
 		int stunden, minuten, sekunden;
-		for (i = 0; i < clock_args->anz_clocks; i++) {
-			dauer = difftime(now, clock_args->clock[i].start_time);
-			x = clock_args->clock[i].x;
-			y = clock_args->clock[i].y;
+		for (i = 0; i < clock_args.anz_clocks; i++) {
+			dauer = difftime(now, clock_args.clock[i].start_time);
+			x = clock_args.clock[i].x;
+			y = clock_args.clock[i].y;
 			stunden = dauer / 3600;
 			minuten = (dauer % 3600) / 60;
 			sekunden = (dauer % 3600) % 60;
@@ -57,50 +79,25 @@ static void *clock_loop(void* _clock_args) {
 		fflush(stdout);
 		sleep(1);
 	}
-	free(clock_args->clock);
+	free(clock_args.clock);
 	return NULL;
 }
 
 /*
  * prepares the parameters for the clock thread and starts it
  */
-static void run_clocks(struct clocks_struct* clock_args, zval* clock_array) {
-	char *key;
-	uint key_len;
-	int array_count;
-	HashTable *arr_hash;
-	HashPosition pointer;
-	zval **data;
-	ulong index;
+static void run_clocks(zval* clock_array) {
 	void* status;
 
 	if (clock_thread != 0) {
 		stop = 1;
 		pthread_join(clock_thread, &status);
 	}
+
 	stop = 0;
 	clock_thread = 0;
-
-	arr_hash = Z_ARRVAL_P(clock_array);
-	array_count = zend_hash_num_elements(arr_hash);
-	clock_args->clock = calloc(array_count, sizeof(struct clock_struct));
-	clock_args->anz_clocks = array_count;
-
-	int i = 0;
-
-	for(zend_hash_internal_pointer_reset_ex(arr_hash, &pointer);
-			zend_hash_get_current_data_ex(arr_hash, (void**) &data, &pointer) == SUCCESS;
-			zend_hash_move_forward_ex(arr_hash, &pointer)) {
-
-		zend_hash_get_current_key_ex(arr_hash, &key, &key_len, &index, 0, &pointer);
-		clock_args->clock[i].start_time = Z_LVAL_PP(data);
-		clock_args->clock[i].x = 62;
-		clock_args->clock[i].y = index + 4;
-		i++;
-	}
-
 	line = 0;
-	pthread_create(&clock_thread, NULL, clock_loop, (void*)clock_args);
+	pthread_create(&clock_thread, NULL, clock_loop, (void*)clock_array);
 }
 
 PHP_METHOD(Clockthread, return2line) {
@@ -113,11 +110,6 @@ PHP_METHOD(Clockthread, return2line) {
 
 PHP_METHOD(Clockthread, stop) {
 	void* status;
-/*
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &clock_thread) != SUCCESS) {
-		return;
-	}
-*/
 	stop = 1;
 	pthread_join(clock_thread, &status);
 
@@ -132,15 +124,22 @@ PHP_METHOD(Clockthread, start)
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "la", &anz_activities, &clock_array) != SUCCESS) {
 		return;
 	}
-
-	clock_args.anz_clocks = 0;
-	clock_args.clock = NULL;
-
-	run_clocks(&clock_args, clock_array);
+	run_clocks(clock_array);
 	RETURN_LONG(clock_thread);
 }
 
-// We give PHP aware of our function, indicating its function table module.
+ZEND_BEGIN_ARG_INFO(arginfo_clockthread_stop, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(arginfo_clockthread_return2line, 0)
+ZEND_ARG_INFO(0, line)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(arginfo_clockthread_start, 0)
+ZEND_ARG_INFO(0, anz_activities)
+ZEND_ARG_INFO(0, clock_array)
+ZEND_END_ARG_INFO()
+
 const zend_function_entry clockthread_functions[] = {
 	PHP_ME(Clockthread, return2line, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
 	PHP_ME(Clockthread, stop, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
@@ -148,7 +147,6 @@ const zend_function_entry clockthread_functions[] = {
 	PHP_FE_END
 };
 
-// We define a function that will cause php when connecting our expansion.
 PHP_MINIT_FUNCTION(clockthread_init)
 {
 	zend_class_entry tmp_clockthread;
